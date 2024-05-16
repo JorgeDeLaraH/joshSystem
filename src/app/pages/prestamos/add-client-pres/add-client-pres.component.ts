0c775bc3a6208a533d5939146d164f59bf81c123
JorgeDeLaraH <93809477+JorgeDeLaraH@users.noreply.github.com>
2024-05-15 23:11:40 -0600

    nuevo agregar cliente prestamos

--- src/app/pages/prestamos/add-client-pres/add-client-pres.component.ts
+++ src/app/pages/prestamos/add-client-pres/add-client-pres.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-client-pres',
   standalone: true,
-  imports: [],
+  imports: [NgClass,FormsModule,CommonModule],
   templateUrl: './add-client-pres.component.html',
   styleUrl: './add-client-pres.component.css'
 })
 export class AddClientPresComponent {
-
+  check:boolean=false
+  capturarCheck(event: Event){
+    if(this.check==false){
+      this.check=true
+    }else{
+      this.check=false
+    }
+    console.log(this.check)
+  }
 }
